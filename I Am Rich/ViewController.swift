ad2b116264dba6d59015051c457f58f346620bca
Siavash <svshjalali@gmail.com>
2020-02-07 22:16:10 +0900

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Siavash Jalali on 2020/02/07.
+//  Copyright © 2020 Shichan Official. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
